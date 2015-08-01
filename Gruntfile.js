f6f54f21eb44effb0668b2743a35c779fcab0d6e
sbmaxx <sbmaxx@gmail.com>
2015-08-01 20:15:37 +0300

    grunt for release helping

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        release: {}
+    });
+
+    grunt.loadNpmTasks('grunt-release');
+
+    grunt.registerTask('default', ['release']);
+
+};
